95818fa177d526168ccbec8707b6831e34851cc2
Neh Joshi <nehjoshi5@gmail.com>
2024-09-05 21:09:28 -0400

    Finished Chat page

--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -1,7 +1,33 @@
+import { Link } from "react-router-dom";
 import "./chatList.css";
 
 export const ChatList = () => {
   return (
-    <div>ChatList</div>
+    <div className="chatList">
+      <span className="title">DASHBOARD</span>
+      <Link to="/dashboard">Create a New</Link>
+      <Link to="/">Explore MAPL AI</Link>
+      <Link to="/">Contact</Link>
+      <hr />
+      <span className="title">DASHBOARD</span>
+      <div className="list">
+        <Link to="/">My chat title</Link>
+        <Link to="/">My chat title</Link>
+        <Link to="/">My chat title</Link>
+        <Link to="/">My chat title</Link>
+        <Link to="/">My chat title</Link>
+        <Link to="/">My chat title</Link>
+        <Link to="/">My chat title</Link>
+        <Link to="/">My chat title</Link>
+      </div>
+      <hr />
+      <div className="upgrade">
+        <img src="/logo.png" alt="" />
+        <div className="texts">
+          <span>Upgrade to MAPL AI Pro</span>
+          <span>Get unlimited access to all features</span>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/routes/dashboardPage/DashboardPage.jsx
+++ src/routes/dashboardPage/DashboardPage.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { useState } from "react";
+import "./dashboardPage.css";
 
 export const DashboardPage = () => {
+
+  const [input, setInput] = useState("");
+
   return (
-    <div>DashboardPage</div>
+    <div className='dashboardPage'>
+      <div className="texts">
+        <div className="logo">
+          <img src="/logo.png" alt="" />
+          <h1>MAPL AI</h1>
+        </div>
+        <div className="options">
+          <div className="option">
+            <img src="/chat.png" alt="" />
+            <span>Create a New Chat</span>
+          </div>
+          <div className="option">
+            <img src="/image.png" alt="" />
+            <span>Analyze Images</span>
+          </div>
+          <div className="option">
+            <img src="/code.png" alt="" />
+            <span>Help me with my code</span>
+          </div>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form>
+          <input onChange={e => setInput(e.target.value)} type="text" placeholder='Ask me anything...' />
+          <button disabled={input.length === 0}>
+            <img src="/arrow.png" alt="" />
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
